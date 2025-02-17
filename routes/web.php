55a079f124cd2d65c64e64b98a4c8d7c559bb2a7
SyaqiraNazaret <snazaretna@gmail.com>
2025-02-17 14:12:15 +0700

    JS02

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+ });
+
+Route::get('/selamat', function () {
+    return 'Selamat Datang';
+ }); 
+
+Route::get('/about', function () {
+    return 'NIM : 2341760123 <br>
+    Nama : Syaqira Nazaretna';
+ });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+    
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan id '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
